b383f3e8297783e96920c0dd87f9453fdfbe85bc
Scott <schepers.scott@hotmail.com>
2018-05-28 19:20:39 +0200

    -Added all the animals

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -10,21 +10,29 @@ public class Animal
     public bool trick { get; set; }
     public string name { get; set; }
     public string helloText { get; set; }
-    public string eatingText { get; set; }
+    public string leaveEatingText { get; set; }
+    public string meatEatingText { get; set; }
 
-    public Animal(string name, bool herbivore, bool carnivore, bool trick, string helloText, string eatingText)
+    public Animal(string name, bool herbivore, bool carnivore, bool trick, string helloText, string leaveEatingText, string meatEatingText)
     {
         this.name = name;
         this.herbivore = herbivore;
         this.carnivore = carnivore;
         this.trick = trick;
         this.helloText = helloText;
-        this.eatingText = eatingText;
+        this.leaveEatingText = leaveEatingText;
+        this.meatEatingText = meatEatingText;
     }
 
 }
 public class Main : MonoBehaviour
 {
-
-
+    private void Start()
+    {
+        Animal hippo = new Animal("Henk", true, false, false, "splash", "munch munch lovely", "");
+        Animal lion = new Animal("Elsa", false, true, false, "roooaoaaaaar", "", "nomnomnom thx mate");
+        Animal pig = new Animal("Dora", true, true, true, "oink oink", "munch munch oink", "nomnomnom oink thx");
+        Animal tiger = new Animal("Wally", false, true, true, "rraaarww", "", "nomnomnom thx wubalubadubdub");
+        Animal zebra = new Animal("Marty", true, false, false, "zebra zebra", "munch munch zank yee bra", "");
+    }
 }
