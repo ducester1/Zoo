119bcf6d4fd014e28af6e4390543b67edf19f84d
Scott <schepers.scott@hotmail.com>
2018-05-28 21:29:51 +0200

    -Changed Main.cs to Animal.cs -Added give leaves

---
+++ Assets/Scripts/Animal.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Zoo
+{
+    public class Animal : MonoBehaviour
+    {
+
+        public GameObject balloon;
+        public bool herbivore;
+        public bool carnivore;
+        [SerializeField]
+        private bool trick;
+        [SerializeField]
+        private string animalName;
+        [SerializeField]
+        private string helloText;
+        [SerializeField]
+        private string leaveEatingText;
+        [SerializeField]
+        private string meatEatingText;
+
+        private void Start()
+        {
+        }
+
+        public void EatLeaves()
+        {
+            if (herbivore == true)
+            {
+                balloon.SetActive(true);
+                balloon.GetComponentInChildren<Text>().text = leaveEatingText;
+            }
+        }
+    }
+}
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -8,18 +8,27 @@ namespace Zoo
     {
         [SerializeField]
         private GameObject lion, hippo, pig, tiger, zebra;
+        private GameObject henk, elsa, dora, wally, marty;
+        private List<GameObject> animals;
         private void Start()
         {
-            Lion henk = Instantiate(lion, transform).GetComponent<Lion>();
-            henk.name = "henk";
-            Hippo elsa = Instantiate(hippo, transform).GetComponent<Hippo>();
-            elsa.name = "elsa";
-            Pig dora = Instantiate(pig, transform).GetComponent<Pig>();
-            dora.name = "dora";
-            Tiger wally = Instantiate(tiger, transform).GetComponent<Tiger>();
-            wally.name = "wally";
-            Zebra marty = Instantiate(zebra, transform).GetComponent<Zebra>();
-            marty.name = "marty";            
+            animals = new List<GameObject>();
+            animals.Add(henk = Instantiate(hippo, transform));
+            animals.Add(elsa = Instantiate(lion, transform));
+            animals.Add(dora = Instantiate(pig, transform));
+            animals.Add(wally = Instantiate(tiger, transform));
+            animals.Add(marty = Instantiate(zebra, transform));
+        }
+
+        private void LeaveEaters()
+        {
+            for (int i = 0; i < animals.Count; i++)
+            {
+                if (animals[i].GetComponent<Animal>().herbivore == true)
+                {
+                    animals[i].GetComponent<Animal>().EatLeaves();
+                }
+            }
         }
     }
 }
